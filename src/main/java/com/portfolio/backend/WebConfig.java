3827c0cb721230df56d3a72fd93615331695b319
MartinMas05 <martinmasetto05@gmail.com>
2023-05-13 10:32:18 -0300

    cors2

--- src/main/java/com/portfolio/backend/WebConfig.java
+++ src/main/java/com/portfolio/backend/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://martinmasetto-ba463.web.app/")
+                .allowedOrigins("https://martinmasetto-ba463.web.app/#/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(false)
