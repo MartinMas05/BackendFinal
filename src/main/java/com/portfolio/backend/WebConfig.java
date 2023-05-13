4091f042013f11837d81f3b8ec4668d6e702b528
MartinMas05 <martinmasetto05@gmail.com>
2023-05-13 12:25:53 -0300

    webconfig bye

--- src/main/java/com/portfolio/backend/WebConfig.java
+++ src/main/java/com/portfolio/backend/WebConfig.java
@@ -1,22 +1,22 @@
 
-package com.portfolio.backend;
+//package com.portfolio.backend;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 // @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(false)
-                .maxAge(3600);
-    }
-}
+//public class WebConfig implements WebMvcConfigurer {
+  //  @Override
+    //public void addCorsMappings(CorsRegistry registry) {
+      //  registry.addMapping("/**")
+        //        .allowedOrigins("*")
+          //      .allowedMethods("GET", "POST", "PUT", "DELETE")
+            //    .allowedHeaders("*")
+              //  .allowCredentials(false)
+                //.maxAge(3600);
+    //}
+//}
 
 
