b40976b7e4faf18083a098b80e3706d9cd725dc8
MartinMas05 <martinmasetto05@gmail.com>
2023-05-13 10:21:38 -0300

    cambiando CORS

--- src/main/java/com/portfolio/backend/WebConfig.java
+++ src/main/java/com/portfolio/backend/WebConfig.java
@@ -2,21 +2,20 @@
 package com.portfolio.backend;
 
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
-
-@EnableWebMvc
 @Configuration
+// @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-   @Override
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("https://martinmasetto-ba463.web.app/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
     }
 }
 
