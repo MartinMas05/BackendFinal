323a13d514745ddde80fedd1dd66f8187d08cc45
MartinMas05 <martinmasetto05@gmail.com>
2023-05-14 17:31:42 -0300

    COMMIT FINAL cors

--- src/main/java/com/portfolio/backend/security/MainSecurity.java
+++ src/main/java/com/portfolio/backend/security/MainSecurity.java
@@ -77,4 +77,16 @@ public class MainSecurity {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Aplica CORS a todas las rutas
+                        .allowedOrigins("*") // Permitir todos los orígenes
+                        .allowedMethods("*"); // Métodos permitidos
+            }
+        };
+    }
 }
